05397acc27d8b6f8b9dfde9feb0528502df4d570
josepc98developer <107946419+josepc98developer@users.noreply.github.com>
2022-06-26 15:32:51 -0400

    Add files via upload

---
+++ comfig.py
@@ -0,0 +1,10 @@
+BOT_TOKEN = '5211163116:AAHhhiAiP2_zdByKtHoRBcM6jILTbR9Dajc'
+API_ID = '16536294'
+API_HASH = '0c780f6db3124fafbdc3dcde437e6f21'
+ADMIN_ID = [1180073584]
+
+MOODLE_URL = 'https://evea.uh.cu/'
+MOODLE_USER = 'darian.borges@estudiantes.fbio.uh.cu'
+MOODLE_PASSWORD = 'darian1995'
+PARTES_MB = 100
+
